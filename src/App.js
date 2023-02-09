e6bd4b341a4e1ad1e6b4863d5144628b6c85c9cf
khangdu <sdawajangbu99@gmail.com>
2023-02-10 02:01:44 +0545

    Login UI in progress

--- src/App.js
+++ src/App.js
@@ -4,9 +4,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <>
+      {/* Nav bar */}
+      {/* <nav>Navigation Bar</nav> */}
+      {/* Nav bar */}
+
+      {/* Main Body  */}
+      <div className="App">
+        {/* Main content */}
+        <div className="content">
+          {/* Main content left side */}
+          <div>
+            <h1>
+              <span>E</span>
+              park
+            </h1>
+            <br />
+            <h3>Park your vehicle on any spot that you like.</h3>
+          </div>
+          {/* Main content right side (Login UI) */}
+          <Login />
+        </div>
+        {/* Bottom information  */}
+        <div className="information">Information</div>
+      </div>
+    </>
   );
 }
 
--- src/components/loginpage.js
+++ src/components/loginpage.js
@@ -1,22 +1,47 @@
-import { Button } from "reactstrap";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import "../design/loginpage.css";
 
 const Login = () => {
-  const handelalert = (e) => {
+  const handlealert = (e) => {
     e.preventDefault();
     window.alert("Reaction completed");
   };
 
   return (
     <>
-      <div className="login">
-        This is the login Page.
-        <span>
-          <Button size="md" color="primary">
-            Login
-          </Button>
-        </span>
-      </div>
+      <Form>
+        <div className="login-box">
+          <FormGroup>
+            <Input
+              name="email"
+              placeholder="Email or Username"
+              bsSize="lg"
+              type="email"
+            ></Input>
+          </FormGroup>
+          <FormGroup>
+            <Input
+              name="password"
+              placeholder="Password"
+              bsSize="lg"
+              type="password"
+              className="password"
+            ></Input>
+          </FormGroup>
+          <FormGroup>
+            <div className="submit">
+              <Button onClick={handlealert} size="lg" id="login">
+                Log in
+              </Button>
+            </div>
+          </FormGroup>
+          {/* Create an account button */}
+          <div className="create-acc">
+            <span id="line"></span>
+            <Button id="new-acc">Create an account</Button>
+          </div>
+        </div>
+      </Form>
     </>
   );
 };
